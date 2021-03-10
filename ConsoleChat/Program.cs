7ce391fbb899fed6a9a45117c2e4e5ea8e9825ab
Микола Матіос <mikolka09@gmail.com>
2021-03-10 20:53:55 +0200

    10.03.2021

---
+++ ConsoleChat/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChat
+{
+    class Program
+    {
+        private static Socket[] clients;
+        private static Socket server;
+        static void Main(string[] args)
+        {
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp.);
+            Task.Run(() =>
+            {
+                while (true)
+                {
+
+                }
+            });
+            Console.ReadKey();
+            server.Close();
+        }
+    }
+}
--- LessonsNP/Program.cs
+++ LessonsNP/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
+using System.Threading;
 
 namespace LessonsNP
 {
@@ -18,27 +19,55 @@ namespace LessonsNP
             try
             {
                 socket.Bind(iPEnd);
-                socket.Listen(10);
-
+                socket.Listen(100);
+                Semaphore semaphore = new Semaphore(20,20);
                 Console.WriteLine("Сервер запущен. Ожидание подключений...");
                 while (true)
                 {
-                    Socket socket1 = socket.Accept();
-                    StringBuilder builder = new StringBuilder();
-                    int bytes = 0;
-                    byte[] data = new byte[256];
-                    do
-                    {
-                        bytes = socket1.Receive(data);
-                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
-                    } while (socket1.Available > 0);
-                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+                    socket.AcceptAsync().ContinueWith((Task<Socket> t)=> //асинхронный метод, работает без ожидания выхода клиента
+                    { 
+                        semaphore.WaitOne();
+                        if(t is null)
+                        {
+                            throw new ArgumentNullException(nameof(t));
+                        }
+                        Socket socketClient = t.Result;
+                        StringBuilder builder = new StringBuilder();
+                        int bytes = 0;
+                        byte[] data = new byte[256];
+                        do
+                        {
+                            bytes = socketClient.Receive(data);
+                            builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                        } while (socketClient.Available > 0);
+                        Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
 
-                    string message = "Ваше сообщение доставлено";
-                    data = Encoding.Unicode.GetBytes(message);
-                    socket1.Send(data);
-                    socket1.Shutdown(SocketShutdown.Both);
-                    socket1.Close();
+                        string message = "Ваше сообщение доставлено";
+                        data = Encoding.Unicode.GetBytes(message);
+                        socketClient.Send(data);
+                        socketClient.Disconnect(true);  //проверяет когда клиент закроется
+                        socketClient.Shutdown(SocketShutdown.Both);
+                        socketClient.Close();
+                        semaphore.Release();
+                    });
+                    Thread.Sleep(10);
+                    //Socket socketClient = socket.Accept();
+                    //StringBuilder builder = new StringBuilder();
+                    //int bytes = 0;
+                    //byte[] data = new byte[256];
+                    //do
+                    //{
+                    //    bytes = socketClient.Receive(data);
+                    //    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    //} while (socketClient.Available > 0);
+                    //Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+
+                    //string message = "Ваше сообщение доставлено";
+                    //data = Encoding.Unicode.GetBytes(message);
+                    //socketClient.Send(data);
+                    ////socketClient.Disconnect(true);  //проверяет когда клиент закроется
+                    //socketClient.Shutdown(SocketShutdown.Both);
+                    //socketClient.Close();
                 }
 
             }

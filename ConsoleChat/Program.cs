3fd3a44229616bedff97417add6b93528a9bb6e8
Микола Матіос <mikolka09@gmail.com>
2021-03-16 20:55:17 +0200

    16.03.2021

--- ConsoleChat/Program.cs
+++ ConsoleChat/Program.cs
@@ -22,57 +22,81 @@ namespace ConsoleChat
         {
             server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPEnd = new IPEndPoint(IPAddress.Parse(ip), port);
+            clients = new List<Client>();
+            lck = new object();
             server.Bind(IPEnd);
             server.Listen(50);
-            lck = new object();
+
+            Console.WriteLine($"Server started: {IPEnd.Address} : {IPEnd.Port}");
+
+
             Task.Run(() =>
             {
                 try
                 {
                     while (true)
                     {
-                        server.AcceptAsync().ContinueWith((Task<Socket> t) =>
+                        Socket socket = server.Accept();
+                        Task.Run(() =>
                         {
-                            Socket socket = t.Result;
-                            byte[] buff = new byte[10];
-                            string name = Encoding.ASCII.GetString(buff);
-                            Client client = new Client() 
-                            { 
+                            byte[] buff = new byte[4];
+                            socket.Receive(buff);
+                            int byteLength = BitConverter.ToInt32(buff, 0);
+                            buff = new byte[byteLength];
+                            socket.Receive(buff);
+                            string name = Encoding.Unicode.GetString(buff);
+                            Client client = new Client()
+                            {
                                 Name = name,
                                 ClientSocket = socket,
                                 ClientIPEnd = (IPEndPoint)socket.LocalEndPoint
                             };
                             clients.Add(client);
+                            Console.WriteLine($"Client connected with Name: {client.Name} and IP: {client.ClientIPEnd.Address}");
+                            SendToEveryone(client, $"{client.Name} joined to chat");
+
                             while (true)
                             {
-                                buff = new byte[1024];
+                                buff = new byte[4];
+                                client.ClientSocket.Receive(buff);
+                                byteLength = BitConverter.ToInt32(buff, 0);
+                                buff = new byte[byteLength];
                                 client.ClientSocket.Receive(buff);
                                 string message = "[" + client.ClientIPEnd.Address + "] "
                                                      + client.Name + ": "
-                                                     + Encoding.ASCII.GetString(buff);
-                                lock (lck)
-                                {
-                                    foreach (var c in clients)
-                                    {
-                                        if (c != client)
-                                        {
-                                            c.ClientSocket.Send(Encoding.ASCII.GetBytes(message));
-                                        }
-                                    }
-                                }
+                                                     + Encoding.Unicode.GetString(buff);
+                                Console.WriteLine($"Message has been recived: {client.Name} and IP: {client.ClientIPEnd.Address}");
+                                SendToEveryone(client, message);
                             }
                         });
-
                     }
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
-               
+
             });
             Console.ReadKey();
+            Console.WriteLine("Server closing...");
             server.Close();
+        }
+
+        private static void SendToEveryone(Client client, string message)
+        {
+            lock (lck)
+            {
+                foreach (var c in clients)
+                {
+                    if (c != client)
+                    {
+                        byte[] buff = Encoding.Unicode.GetBytes(message);
+                        c.ClientSocket.Send(BitConverter.GetBytes(buff.Length));
+                        c.ClientSocket.Send(buff);
+                    }
+                }
+                Console.WriteLine($"Message has been sent: {client.Name} and IP: {client.ClientIPEnd.Address}");
+            }
         }
     }
 
--- ConsoleChatClient/Program.cs
+++ ConsoleChatClient/Program.cs
@@ -1,15 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace ConsoleChatClient
 {
     class Program
     {
+        private static Socket client;
+        private static IPEndPoint IPEnd;
+
+        private static string ip = "127.0.0.1";
+        private static int port = 8081;
+        private static string message = "";
+        private static string name = "";
+
         static void Main(string[] args)
         {
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEnd = new IPEndPoint(IPAddress.Parse(ip), port);
+            client.Connect(IPEnd);
+
+            Console.Write("Enter Name: ");
+            name = Console.ReadLine();
+            byte[] buff = Encoding.Unicode.GetBytes(name);
+            client.Send(BitConverter.GetBytes(buff.Length));
+            client.Send(buff);
+
+           ReciveAsync();
+
+            while (!message.Equals("exit"))
+            {
+                Console.Write($"[{IPEnd.Address}] {name}: ");
+                message = Console.ReadLine();
+               
+                buff = Encoding.Unicode.GetBytes(message);
+                client.Send(BitConverter.GetBytes(buff.Length));
+                client.Send(buff);
+            }
+
+            
+        }
+
+        private static async void ReciveAsync()
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    byte[] buff1 = new byte[4];
+                    client.Receive(buff1);
+                    int byteLength = BitConverter.ToInt32(buff1, 0);
+                    buff1 = new byte[byteLength];
+                    client.Receive(buff1);
+                    string message = Encoding.Unicode.GetString(buff1);
+                    Console.WriteLine(message);
+                }
+            });
         }
     }
 }
